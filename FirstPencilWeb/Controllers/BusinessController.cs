7bfb4f757867bc8bc6367e70bfd7ccff34e0721a
anjijsb <luchen@anjismart.com>
2015-05-04 14:20:11 +0800

    201505041420

---
+++ FirstPencilWeb/Controllers/BusinessController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstPencilWeb.Controllers
+{
+    public class BusinessController : Controller
+    {
+        // GET: Business
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 拍卖详情页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Auction()
+        {
+            return View();
+        }
+    }
+}
