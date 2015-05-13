6b2d5fa580b648c139ea8cbb8711c7faa19a414c
anjijsb <luchen@anjismart.com>
2015-05-13 17:42:59 +0800

    201505131742

--- FirstPencilWeb/Controllers/UsersController.cs
+++ FirstPencilWeb/Controllers/UsersController.cs
@@ -1,9 +1,13 @@
 ﻿using FirstPencilWeb.Helps;
+using FirstPencilWeb.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace FirstPencilWeb.Controllers
 {
@@ -32,6 +36,11 @@ namespace FirstPencilWeb.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 获取openid
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
         public ActionResult DealershipsOpenId(string code)
         {
             if (code != null && code != "")
@@ -47,5 +56,25 @@ namespace FirstPencilWeb.Controllers
 
         }
 
+        /// <summary>
+        /// 签到
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult DealerSgin()
+        {
+            HttpClient client = new HttpClient();
+            var cl = client.GetStringAsync(string.Format("{0}api/Salesman/GetFirms", System.Web.Configuration.WebConfigurationManager.AppSettings["fpsip"])).Result;
+            List<Deler> d = JsonHelp.todui<List<Deler>>(cl);
+            return View(d);
+        }
+    }
+
+    public static class JsonHelp
+    {
+        public static T todui<T>(string json)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+        }
     }
 }
+
---
+++ FirstPencilWeb/Models/Deler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstPencilWeb.Models
+{
+    public class Deler
+    {
+        public int FirmId { get; set; }
+        public string Name { get; set; }
+        public string Area { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Remarks { get; set; }
+    }
+}
