0aea37818bddf92e862be3f703e9cd7ac5c7308e
piero7 <piero_chen@163.com>
2015-05-19 10:58:59 +0800

    add get access token

---
+++ FirstPencilService/Controllers/HelperController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FirstPencilService.Controllers
+{
+    public class HelperController : ApiController
+    {
+        /// <summary>
+        /// 获取accesstoken
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string GetAccessToken()
+        {
+            return WechatHelper.GetToken();
+        }
+    }
+}
